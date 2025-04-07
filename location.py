a858f2e993aef3187fbc0e5445f206e5dd2f6c3d
mkbuczek <buczekkmason@gmail.com>
2025-04-07 00:37:29 -0400

    v2
    updated code framework

--- location.py
+++ location.py
@@ -1,16 +1,22 @@
 class Location:
 
-    def __init__(self, country, state, city):
-        self.country = country
-        self.state = state
-        self.city = city
-
     #search input location (from frontend) using api
-    def searchLocation():
+    def searchLocation(self):
+        searchStatus = False
+        results = ''
+
+        #search the db for the input city
+        #if it is found, call displaySearchResults, "searchStatus = true"
+        #if it is not found, "searchStatus = false"
+        #pass "results" as an array/object so we can pass to frontend and iterate through the list...
+        #to display each result (if you search "Mi" it will display Michigan, Minnesota, etc.)
+        #feel free to delete this before coding
+
+        self.displaySearchResults(searchStatus, results)
         pass
 
-    #send search results to frontend
-    def displaySearchResults():
+    #send search results to frontend (is the city found or not)
+    def displaySearchResults(self, searchStatus, results):
         pass
 
         
