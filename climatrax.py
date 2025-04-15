4a625b094addbdd11207c403bd71b87e34ee1025
mkbuczek <buczekkmason@gmail.com>
2025-04-15 14:33:03 -0400

    merge philip branch

--- climatrax.py
+++ climatrax.py
@@ -7,13 +7,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/news')
+def news():
+    return render_template('news.html')
 
-@app.route('/get_location',methods =['GET'])
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
+
+@app.route('/location')
+def location():
+    return render_template('location.html')
+
+
+
+# @app.route('/get_location',methods =['GET'])
+# def get_location():
+#     query = request.args.get('query')
+#     result, status_code = search_location(query)
+#     return jsonify(result), status_code
+@app.route('/get_location', methods=['GET'])
 def get_location():
     query = request.args.get('query')
+    print(f"Debug: Received query {query}")  # Debug statement
     result, status_code = search_location(query)
     return jsonify(result), status_code
-
 
 
 if __name__ == "__main__":
