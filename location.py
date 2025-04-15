4b3a2589293e0bd83b55d625ff3f57bee78645f2
mkbuczek <buczekkmason@gmail.com>
2025-04-15 15:06:04 -0400

    updated file paths in html, stills needs api debugging

--- location.py
+++ location.py
@@ -1,4 +1,5 @@
 import json
+from flask import jsonify
 
 """This function is tied to the /get_location end
    Parameters = Country, State or city
@@ -31,22 +32,6 @@ def search_location(query):
             return jsonify(result)
         
     return ({"error": "Location not supported"}), 404
-
-
-    #search input location (from frontend) using api
-    def searchLocation(self):
-        searchStatus = False
-        results = ''
-
-        #search the db for the input city
-        #if it is found, call displaySearchResults, "searchStatus = true"
-        #if it is not found, "searchStatus = false"
-        #pass "results" as an array/object so we can pass to frontend and iterate through the list...
-        #to display each result (if you search "Mi" it will display Michigan, Minnesota, etc.)
-        #feel free to delete this before coding
-
-        self.displaySearchResults(searchStatus, results)
-        pass
 
     #send search results to frontend (is the city found or not)
     def displaySearchResults(self, searchStatus, results):
--- static/api.js
+++ static/api.js
@@ -1,12 +1,7 @@
+//search form is the search box where users search for a country
 const searchForm = document.getElementById("searchForm");
-
-//event listener for the search form
-searchForm.addEventListener("submit", function(event) {
-    event. preventDefault(); //prevents a page refresh every submission
-
-    const userInput = document.getElementById("searchInput").value; //user's search input
-    console.log("User searched:", userInput);
-})
+//result container will hold all / some results
+const resultContainer = document.getElementById('resultContainer');
 
 //html onclick calls this; redirects to each location's weather page
 function redirect(location)
@@ -14,51 +9,60 @@ function redirect(location)
     window.location.href = `/location/${location}`;
 }
 
-//this function fetches location data from Flask API
-
-async function getLocation(query){
-    try{
-        // make the GET request
-        const response = await fetch('/get_location?query=${encodeURIComponent(query)}',{
-            method: 'GET',
-            headers: {
-                'Content_Type' : 'application/json'
-            }
-        });
-        if(!response.ok){
-            throw new Error('HTTP error! status: ${response.status}')
-        }
-    }catch (error){
-        console.error('Error fetching location:',error)
-        return { error: error.message}; 
-    }
-
-}
-
 //the flow is getLocation => searchLocation => displaySearchResults => fetchResults => displayResults
 
-//reads location input by user and passes it to location.py
+//Get location starts it all off.
+//it accepts values from the text box and call searchLocation() function
 function getLocation()
 {
-    //i suggest setting up flask and using static test values ("Michigan, Tokyo, Jakarta, etc.")
-    //connectivity with the website's buttons can be added once the html is set up
-    //feel free to delete this before coding
+    const userInput = document.getElementById("searchInput").value;
+    if(userInput){
+        searchLocation(userInput);
+        console.log("User searched:", userInput);
+
+    }else{
+        console.log("Error retrieving input from user!");
+    }
+}
+
+function searchLocation(query)
+{
+    fetchResults(query);
 }
 
 //fetches search results from the backend to display on the website
-function fetchResults()
+function fetchResults(query)
 {
-    //code this after location.py implementation
-    //fetch search results and status from location.py and pass to displayResults
-    //feel free to delete this before coding
-    displayResults(searchStatus, results)
+    console.log("Fetching results of query")
+    fetch(`/get_location?query=${encodeURIComponent(query)}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("API response:", data);
+        displayResults(data);
+    })
+    .catch(error => console.error("Fetch error:", error));
+    //displayResults(searchStatus, results)
 }
 
 function displayResults(searchStatus, results)
 {
+
+
+
+
     //code this after fetchResults implementation
     //if searchStatus=true (the location was found), display search results
     //iterate through search results to display a DOM element per result
     //feel free to delete this before coding
 }
-
+//waits for the submit button to be clicked
+//calls getlocation() 
+searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    getLocation(); 
+});
